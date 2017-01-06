238d7595a44368519621a6b4276605dd1236f3eb
Chauhan Akash(チョウハン　アカーシュ) <ch.m.aakash@gmail.com>
2017-01-06 02:05:51 +0000

    Going old school with #HashLocationStrategy : not recommended

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './landing.template.html',
   styleUrls: ['./landing.style.css']
 })
 export class Landing {
+  
+  public toggleSmallMenuView : boolean;
+  
+  constructor(private route: ActivatedRoute) {
+    this.toggleSmallMenuView = false;
+  }
+  
+  toggleFunction() {
+    this.anchorClicked();
+    this.toggleSmallMenuView = !this.toggleSmallMenuView;
+  }
+  
+  anchorClicked() {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( "#" + f );
+      if ( element ) element.scrollIntoView ( element );
+    });
+  }
   
 }
